b0cc4f485b5be25b60c4860a4d224723964007ad
JonatanMilver <48964784+JonatanMilver@users.noreply.github.com>
2021-04-24 18:55:35 +0300

    added about modal

---
+++ js/about.js
@@ -0,0 +1,34 @@
+function toggleAbout(){
+// Get the modal
+    var modal = document.getElementById("about-modal");
+
+    console.log(modal)
+    // Get the button that opens the modal
+    var btn = document.getElementById('aboutMenu');
+    console.log(btn)
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    document.onkeydown = function(event) {
+        if(event.which == 27){
+            modal.style.display = "none";
+        }
+    }
+}
--- js/settings.js
+++ js/settings.js
@@ -4,7 +4,10 @@ var rightArrow = 39;
 var upArrow = 38;
 
 $( function() {
-    $( "#accordion" ).accordion();
+    $( "#accordion" ).accordion({
+        collapsible: true,
+        heightStyle: "content"
+    });
   } );
 
 
@@ -15,8 +18,8 @@ $("#datepicker").datepicker();
 
 
 $( function() {
-    var handle = $( "#custom-handle" );
-    $( "#slider" ).slider({
+    let handle = $( "#food-custom-handle" );
+    $( "#food-slider" ).slider({
         min:50,
         max:90,
         value: 50,
@@ -28,6 +31,22 @@ $( function() {
         }
     });
   } );
+
+$( function() {
+    let handle = $( "#monster-custom-handle" );
+    $( "#monster-slider" ).slider({
+        min:1,
+        max:4,
+        value: 2,
+        create: function() {
+            handle.text( $( this ).slider( "value" ) );
+        },
+        slide: function( event, ui ) {
+            handle.text( ui.value );
+        }
+    });
+} );
+
 $(function() {
     $('#color-picker1').spectrum({
         type: "text",
@@ -66,4 +85,40 @@ function leftMove(e){
 function upMove(e){
     upArrow = e.which;
     upMoveInput.value = e.key;
+}
+
+function randomizeSettings(){
+    //randomize num of monsters
+    let randomMonstersNum = Math.floor(Math.random() * 4) + 1;
+    $('#monster-slider').slider("option", "value", randomMonstersNum);
+    $('#monster-custom-handle').text(randomMonstersNum);
+    //monsters speed
+    let speedOptions = ['8', '6', '4']; // 8 - Slow, 6 - Medium, 4 - High
+    let randomSpeed = Math.floor(Math.random() * 3);
+    speed.value = speedOptions[randomSpeed];
+    console.log(speed.value);
+    //food colors
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    $('#color-picker1').spectrum('set', randomColor);
+    randomColor =  Math.floor(Math.random()*16777215).toString(16);
+    $('#color-picker2').spectrum('set', randomColor);
+    randomColor =  Math.floor(Math.random()*16777215).toString(16);
+    $('#color-picker3').spectrum('set', randomColor);
+    // game time
+    let randomTime = Math.floor(Math.random()*240) + 60;
+    gameTime.value = randomTime;
+    // food amount
+    let randomFoodAmt = Math.floor(Math.random()*40) + 50;
+    $('#food-slider').slider("option", "value", randomFoodAmt);
+    $('#food-custom-handle').text(randomFoodAmt);
+}
+
+function gameTimeValidation(){
+    let input = gameTime;
+    if(parseInt(input.value) < 60){
+        input.value = 60;
+    }
+    else if(parseInt(input.value) > 300){
+        input.value = 300;
+    }
 }
